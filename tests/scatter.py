f040abf54cea490c75ad09bbf0c78dfe7af92d6f
agbrothers <greysonbrothers@gmail.com>
2026-05-15 14:01:01 -0400

    Add basic test-case mockups to clarify what we want the user experience to look like

---
+++ tests/scatter.py
@@ -0,0 +1,20 @@
+# ---------------------- Expressions ----------------------
+
+### This should automatically create a scatter plot of d
+d
+
+### This should automatically create a scatter plot of d_rand
+d_rand
+
+### Plot a surface parametrized by a point created in data
+z = dot([x,y], d[2])
+
+
+# ---------------------- Data ----------------------
+
+### Define the data 
+d = array([[0,1], [1,0], [-1, -1]])
+
+
+### Here random is directly imported from np.random
+d_rand = random((4, 2))
---
+++ tests/surface.py
@@ -0,0 +1,29 @@
+# ---------------------- Expressions ----------------------
+
+### Plot the line
+y = x ** 3
+
+### DEFINE CONSTANTS
+a = 0.6
+b = 0.5
+p = 0.7
+
+### Plot the constrained surface
+z = a*x**2 + b*y**2
+constraint: x**2 + y**2 < 1
+constraint: x < 0.5
+constraint: x > -0.5
+
+### Plot the compositional surface
+z = p*f(x,y) + (1-p)*g(x,y)
+
+
+f(x,y) =  x**2 + y**2
+
+
+g(x,y) = -x**2 - y**2
+
+
+# ---------------------- Data ----------------------
+
+
